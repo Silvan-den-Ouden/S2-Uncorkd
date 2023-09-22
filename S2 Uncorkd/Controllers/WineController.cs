ea53a0300457078e7bc56bd8fe13d3de8ef0035b
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2023-09-22 15:20:03 +0200

    silly little fix (mapped wineries correctly)

--- S2 Uncorkd/Controllers/WineController.cs
+++ S2 Uncorkd/Controllers/WineController.cs
@@ -11,17 +11,12 @@ namespace S2_Uncorkd.Controllers
         private readonly WineCollection _wineCollection = new();
         private readonly WineryCollection _wineryCollection = new();
         private readonly TasteTagCollection _tasteTagCollection = new();
-        //public IActionResult Index()
-        //{
-        //    List<WineModel> wineModels = _wineCollection.GetWines();
-
-        //    return View(wineModels);
-        //}
+        
 
         public IActionResult Index(int ID)
         {
             WineModel wineModel = _wineCollection.GetWineWithID(ID);
-            WineryModel wineryModel = _wineryCollection.GetWineryWithID(wineModel.Id);
+            WineryModel wineryModel = _wineryCollection.GetWineryWithID(wineModel.Winery_id);
             List<TasteTagModel> tasteTagModels = _tasteTagCollection.GetTagFromWineID(wineModel.Id);
 
             WineViewModel wineViewModel = new(wineModel, wineryModel, tasteTagModels);
