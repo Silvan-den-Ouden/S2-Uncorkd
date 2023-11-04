63cb081e5294d9748ab359d88a097c5bf619ffaf
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2023-11-04 15:46:52 +0100

    begin registratie

---
+++ S2 Uncorkd/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S2_Uncorkd.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
